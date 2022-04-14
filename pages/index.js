4ac96fc31d27f54b5409129b307b27a5a7863852
udhaycomorins <udhay@comorins.com>
2022-04-14 14:04:13 +0530

    fifth

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import {useState} from 'react'
 
 export default function Home() { 
   const [count,setcount] = useState(0)
+  const [trueState,settrueState] = useState(false)
   const bg = ["bgm","bgm2","bgm3","bgm4","bgm5",'bgm6']
   const im = ['im','im2','im3','im4','im5']
-  const content = ['At Gandhi Memorial', 'At saravan stores', 'We both', 'At temple', 'Final foot prints on we met last']
+  const content = ['At Gandhi Memorial', 'At saravan stores', 'Your pick', 'At temple', 'Final foot prints on we met last']
   const clickHandler = (key) => {     
       if(key === "add"){
         if(count < 5){
@@ -25,33 +26,46 @@ export default function Home() {
 
   console.log("count",count)
   return (
-    <div className="top_container">      
-      <div className={`w-100 ${bg[count]}`}>        
-        <div style={{position:'relative',height:'100vh'}}>
-          <div className='img-pos'>
-            {count < 5 ?
-            <div>
-              <img className='img-fluid img-pad' src={`img/${im[count]}.jpeg`}></img> 
-            </div> : <div>
-                <video className='w-100' controls autoPlay style={{padding:10}}>
-                  <source src='img/bdaycelebration.mp4' type="video/mp4"></source>
-                </video>
-                <p className='no-margin p-style'>At your Bday celebration</p>
-              </div>
-            }
-            {count < 5 &&
-              <p className='no-margin p-style'>{content[count]}</p> 
-            }                          
-          </div>            
-          <div className='d-flex align-items-center justify-content-around left-pos' style={{position:'absolute',height:100,width:100}}>
-            <p className='no-margin' onClick={()=>clickHandler("sub")}>Click</p>
-          </div>
-          <div className='d-flex align-items-center justify-content-around right-pos' style={{position:'absolute',height:100,width:100}}>
-            <p className='no-margin' onClick={()=>clickHandler("add")}>Click</p>
+    <div className="top_container">
+      <audio autoPlay>
+        <source src="img/naanpizhai.mp3" type="audio/mpeg" />  
+      </audio>    
+      {trueState ?
+        <div className={`w-100 ${bg[count]}`}>        
+          <div style={{position:'relative',height:'100vh'}}>
+            <div className='img-pos'>
+              {count < 5 ?
+              <div>
+                <img className='img-fluid img-pad' src={`img/${im[count]}.jpeg`}></img> 
+              </div> : <div>
+                  <video className='w-100' controls autoPlay style={{padding:10}}>
+                    <source src='img/bdaycelebration.mp4' type="video/mp4"></source>
+                  </video>
+                  <p className='no-margin p-style'>At your Bday celebration</p>
+                </div>
+              }
+              {count < 5 &&
+                <p className='no-margin p-style'>{content[count]}</p> 
+              }                          
+            </div>            
+            <div className='d-flex align-items-center justify-content-around left-pos' style={{position:'absolute',height:100,width:100}}>
+              <p className='no-margin' onClick={()=>clickHandler("sub")}>Click</p>
+            </div>
+            <div className='d-flex align-items-center justify-content-around right-pos' style={{position:'absolute',height:100,width:100}}>
+              <p className='no-margin' onClick={()=>clickHandler("add")}>Click</p>
+            </div>          
+            <p className='no-margin p-style' style={{fontSize:28}}>Happy Bday Brintha</p>          
           </div>          
-          <p className='no-margin p-style' style={{fontSize:28}}>Happy Bday Brintha</p>          
-        </div>          
-      </div>                    
+        </div> : 
+        <div className='linear' style={{position:"relative",height:'100vh'}}> 
+          <div className='initial_pos'>
+            <p className='no-margin text-center' style={{paddingBottom:20 }}>Udhayakumar</p>
+            <div className='init_click' onClick={()=>settrueState(true)}>
+              <p className='no-margin text-center'>Click</p>
+            </div>  
+          </div>
+        </div> 
+      }                    
     </div>            
   )
 }
